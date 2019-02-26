280ee8fd56c096d4617d05673951e5652c4b464f
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-02-26 20:30:23 +0100

    Added solution for task: Сумма двух чисел

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,13 +1,9 @@
-/**
- * Функция, складывающая два числа
- * @param {number} a - первый параметр
- * @param {number} b - второй параметр
- * @throws {TypeError} - Бросает ошибку если один из параметров не число
- * @returns {number} - сумма параметров
- */
-
 function sum(a, b) {
-  /* ваш код здесь */
+  if ([a, b].some((value) => typeof value !== 'number')) {
+    throw new TypeError();
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
